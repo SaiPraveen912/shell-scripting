0705949bfd574f74d7c35869c24f7a002177d9cc
SaiPraveen912 <saipraveen.immanni@gmail.com>
2025-04-18 12:14:44 +0530

    hello script

--- 05-variables.sh
+++ 05-variables.sh
@@ -2,10 +2,12 @@
 
 echo "Please enter username::"
 
-read USERNAME #here USERNAME is variable
+# read USERNAME #here USERNAME is variable
+read -s USERNAME # -s --> don't display what we enter
 
 echo "Please enter password::"
 
-read PASSWORD #here PASSWORD is another variable
+# read PASSWORD #here PASSWORD is another variable
+read -s PASSWORD # -s --> don't display what we enter
 
 echo "Username is: $USERNAME, Password is: $PASSWORD"
