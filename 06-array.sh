ab53a5b4150812483f2c737d3b4f09fe481b7506
SaiPraveen912 <saipraveen.immanni@gmail.com>
2025-04-18 12:27:05 +0530

    hello script

--- 06-array.sh
+++ 06-array.sh
@@ -9,3 +9,4 @@ MOVIES=("RRR" "DJTillu" "Devara") # list of values
 echo "First movie is ${MOVIES[0]}"
 echo "Second movie is ${MOVIES[1]}"
 echo "Third movie is ${MOVIES[2]}"
+echo "List of all movies is ${MOVIES[@]}"
