108c79f3db811e3319b9a390ee164f4c034214a1
SaiPraveen912 <saipraveen.immanni@gmail.com>
2025-04-20 09:48:49 +0530

    004-variables

--- 004-variables.sh
+++ 004-variables.sh
@@ -3,5 +3,5 @@
 PERSON1=$1
 PERSON2=$2
 
-echo "$PERSON1:: Hi $PERSON2 how are you"
-echo "${PERSON!}:: Hi ${PERSON2} how are you"
+echo "$PERSON1:: Hi $PERSON2 how are you?"
+echo "$PERSON2:: Hi $PERSON1 I am fine, what about you?"
