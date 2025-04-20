1a2a24f8c98037ae59061b398f0cf73b4c68b927
SaiPraveen912 <saipraveen.immanni@gmail.com>
2025-04-20 10:10:04 +0530

    arrays

--- 06-array.sh
+++ 06-array.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-MOVIES=("RRR" "DJTillu" "Devara") # list of values
-
-# Size of above array is 3.
-# Indexes are 0,1,2
-# list always start with 0.
+MOVIES=("RRR" "DEVARA" "DRAGON")
 
 echo "First movie is ${MOVIES[0]}"
 echo "Second movie is ${MOVIES[1]}"
 echo "Third movie is ${MOVIES[2]}"
-echo "List of all movies is ${MOVIES[@]}"
+echo "List of all movies ${MOVIES[@]}"
