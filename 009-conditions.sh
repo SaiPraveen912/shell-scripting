9f34cfd23284cfe3b6c6062b81b80fdf233ce320
SaiPraveen912 <saipraveen.immanni@gmail.com>
2025-04-21 09:02:31 +0530

    009-conditions

--- 009-conditions.sh
+++ 009-conditions.sh
@@ -4,9 +4,7 @@ NUMBER=$1
 
 if [ $NUMBER -gt 10 ]
 then
-    echo "Given number $NUMBER is grater than 10"
+    echo "Given number $NUMBER is greatr than 10"
 else
     echo "Given number $NUMBER is less than 10"
 fi
-
-
