b08c3b2f313ba9c0c11114f14f06866d40bbd004
SaiPraveen912 <saipraveen.immanni@gmail.com>
2025-04-21 09:37:38 +0530

    010-istallation

--- 010-installation.sh
+++ 010-installation.sh
@@ -5,10 +5,19 @@ USER=$(id -u)
 if [ $USER -ne 0 ]
 then
     echo "Please run this script with root access"
+    exit 1 # Manually exit if error comes
 else
     echo "You are super user"
 fi
 
 dnf install mysql -y
+
+if [ $? -ne 0 ]
+then 
+    echo "Installation of MySQL...FAILURE"
+    exit 1
+else
+    echo "Installation of MySQL...SUCCESS"
+fi
 
 echo "Is script proceeding?"
