da69bf8f625357c196a2b3fc8dc0d45823e8f356
SaiPraveen912 <saipraveen.immanni@gmail.com>
2025-04-22 10:06:50 +0530

    colors

--- 13-colors.sh
+++ 13-colors.sh
@@ -52,5 +52,5 @@ VALIDATE $? "Installing Git"
 #     echo "Installation of Git...SUCCESS"
 # fi
 
-dnf install dockerr -y &>> $LOGFILE
+dnf install docker -y &>> $LOGFILE
 VALIDATE $? "Installing Docker"
