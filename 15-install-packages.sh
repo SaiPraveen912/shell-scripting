74225b328530899c9324d9fc11d622897c78ecf2
SaiPraveen912 <saipraveen.immanni@gmail.com>
2025-04-22 11:05:59 +0530

    install-packages

--- 15-install-packages.sh
+++ 15-install-packages.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 USER=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
 if [ $USER -ne 0 ]
 then
     echo "Please run this script with super user access"
-    exit 1
+    exit 1 # Manually exit if error comes
 else 
     echo "You are super user"
 fi
@@ -13,5 +16,5 @@ fi
 for i in $@
 do 
     echo "Package to insatll: $i"
-    dnf list installed $i
+    dnf list installed $i &>> $LOGFILE
 done
