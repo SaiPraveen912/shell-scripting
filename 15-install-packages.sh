a3f6604897d008a340439e8651365029519d4ad3
SaiPraveen912 <saipraveen.immanni@gmail.com>
2025-04-22 11:15:11 +0530

    install-packages

--- 15-install-packages.sh
+++ 15-install-packages.sh
@@ -5,6 +5,16 @@ TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2...FAILURE"
+        exit 1
+    else
+        echo "$2...SUCCESS"
+    fi
+}
+
 if [ $USER -ne 0 ]
 then
     echo "Please run this script with super user access"
@@ -13,8 +23,12 @@ else
     echo "You are super user"
 fi
 
-for i in $@
+for i in $@ # $@ all variables passed in command line like --> mysql, git etc packages
 do 
     echo "Package to insatll: $i"
-    dnf list installed $i &>> $LOGFILE
+    dnf list installed $i &>>$LOGFILE
+    if [ $? -eq 0 ]
+    then
+        echo "$i already installed...SKIPPING"
+    fi
 done
