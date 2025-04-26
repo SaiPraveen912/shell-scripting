763d79549a9f505b9e01e4f0af5c62f496cdba4b
SaiPraveen912 <saipraveen.immanni@gmail.com>
2025-04-26 11:42:18 +0530

    15-Install-Package

--- 15-install-package.sh
+++ 15-install-package.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
 USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2...FAILURE"
+        exit 1
+    else
+        echo "$2...SUCCESS"
+    fi
+}
 
 if [ $USERID -ne 0 ]
 then
@@ -13,4 +26,12 @@ fi
 for i in $@
 do
     echo "Package to install: $i"
+    dnf list installed $i &>>$LOGFILE  # exit status of this command will be 0
+    if [ $? -eq 0 ]
+    then
+        echo "$i already installed...SKIPPING"
+        VALIDATE $? "Installation of $i"
+    else
+        dnf install $i -y &>>$LOGFILE
+    fi
 done
