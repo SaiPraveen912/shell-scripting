d70c117c597a4c20976c447277a1272696361336
SaiPraveen912 <saipraveen.immanni@gmail.com>
2025-04-26 11:22:38 +0530

    15-Install-Package

---
+++ 15-install-package.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run the script with root user"
+    exit 1
+else
+    echo "You are super user"
+fi
+
+echo "All Packages: $@"
